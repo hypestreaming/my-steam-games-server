c0e85421a6feba85a688da357b42cb46a7c8341b
Gil Megidish <gil@megidish.net>
2022-11-20 09:30:54 +0200

    Added TWITCH_EXTENSION_SECRET

--- config.ts
+++ config.ts
@@ -1,9 +1,10 @@
 export const config = {
 	steam: {
-		key: '$STEAM_KEY',
+		key: "$STEAM_KEY",
 	},
 	twitch: {
 		clientId: "wl1d1suog3vnmhkrhwow16yfwje9u6",
+		extensionSecret: "$TWITCH_EXTENSION_SECRET",
 	}
 };
 
